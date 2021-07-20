9e15e33bf577733593ed1dcb3c5651970fdb25ec
Jose Johnson <jose.johnson.jose@gmail.com>
2021-07-20 18:37:05 +0800

    feat: implemented joystick and toggle buttons

--- lib/pages/remote_control.dart
+++ lib/pages/remote_control.dart
@@ -1,4 +1,8 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:lionsbot_test/widgets/joystick.dart';
 
 
 class RemotePage extends StatefulWidget {
@@ -13,6 +17,8 @@ class RemotePage extends StatefulWidget {
 class _RemotePageState extends State<RemotePage> {
   TextEditingController _emailController = TextEditingController();
   TextEditingController _passwordController = TextEditingController();
+  bool waterToggle = false;
+  bool brushToggle = false;
 
   @override
   void initState() {
@@ -26,11 +32,14 @@ class _RemotePageState extends State<RemotePage> {
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0.0,
-        title: Center(
-          child: Text("Remote",style: TextStyle(
+        centerTitle: true,
+        title: Text(
+          "Remote Control",
+            style: TextStyle(
             color: Colors.white,
-            fontSize: 40),),
-        ),
+            fontSize: 40,
+            fontWeight: FontWeight.bold),
+          ),
         leading: IconButton(
           icon: Icon(Icons.arrow_back_ios),
           onPressed: (){
@@ -57,46 +66,147 @@ class _RemotePageState extends State<RemotePage> {
                     height: 150,
                   ),
                   Container(
-                    padding: EdgeInsets.all(10),
-                    // height: 200,
-                    child: new TextField(
-                      controller: this._emailController,
-                      decoration: InputDecoration(
-                        filled: true,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(10.0),
-                          )
+                    height: 50,
+                    width: 350,
+                    decoration: BoxDecoration(
+                      border: Border(
+                        top: BorderSide(
+                          style: BorderStyle.solid,
+                          color: Colors.white
                         ),
-                        hintText: 'Something',
-                        hintStyle: TextStyle(color: Colors.grey),
-                        fillColor: Colors.white),
+                        bottom: BorderSide(
+                          style: BorderStyle.solid,
+                          color: Colors.white
+                        ), 
+                      )
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.water_rounded,
+                          color: Colors.white,
+                        ),
+                        SizedBox(
+                          width: 10
+                        ),
+                        Text(
+                          "WATER",
+                          style: TextStyle(
+                            color: Colors.white
+                          ),
+                        ),
+                        SizedBox(
+                          width: 10
+                        ),
+                        GestureDetector(
+                          onTap: (){
+                              setState(() {
+                                this.waterToggle= !this.waterToggle;
+                              });
+                          },
+                          child: Container(
+                            padding: EdgeInsets.zero,
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Colors.white,
+                              ),
+                              borderRadius: BorderRadius.circular(100)
+                            ),
+                            child: CupertinoSwitch(
+                              activeColor: Colors.lightBlue,
+                              value: this.waterToggle, 
+                              onChanged: (bool value) {
+                                setState(() {
+                                  this.waterToggle = value;
+                                });
+                              },
+                            ),
+                          ),
+                        ),
+                        VerticalDivider(
+                          color: Colors.white,
+                        ),
+                        SizedBox(
+                          width: 10
+                        ),
+                        Icon(
+                          Icons.brush, 
+                          color: Colors.white,
+                        ),
+                        SizedBox(
+                          width: 10
+                        ),
+                        Text(
+                          "BRUSH",
+                          style: TextStyle(
+                            color: Colors.white
+                          ),
+                        ),
+                        SizedBox(
+                          width: 10
+                        ),
+                        GestureDetector(
+                          onTap: (){
+                              setState(() {
+                                this.brushToggle= !this.brushToggle;
+                              });
+                          },
+                          child: Container(
+                            padding: EdgeInsets.zero,
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Colors.white,
+                              ),
+                              borderRadius: BorderRadius.circular(100)
+                            ),
+                            child: CupertinoSwitch(
+                              activeColor: Colors.lightBlue,
+                              value: this.brushToggle, 
+                              onChanged: (bool value) {
+                                setState(() {
+                                  this.brushToggle = value;
+                                });
+                              },
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  Container(
-                    padding: EdgeInsets.all(10),
-                    child: new TextField(
-                      controller: this._passwordController,
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        filled: true,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(10.0),
-                          )
-                        ),
-                        // labelText: 'Password',
-                        hintText: 'Else',
-                        hintStyle: TextStyle(color: Colors.grey),
-                        fillColor: Colors.white,
-                        suffixIcon: TextButton(
-                          onPressed: () { print("Unobscure password"); },
-                          child: Text("Show"),
-                        ),
-                      ),
-                    ),
-                  ),
-                  
                 ],
               ),
+              Joystick(),
+              // Stack(
+              //   children: [
+              //     Container(
+              //       width: MediaQuery.of(context).size.width/1.5,
+              //       height: MediaQuery.of(context).size.width/1.5,
+              //       decoration: BoxDecoration(
+              //         // borderRadius: BorderRadius.circular(100),
+              //         color: Color(0xFF003366),
+              //         shape: BoxShape.circle,
+                      
+              //       ),
+              //     ),
+              //     Container(
+              //       width: MediaQuery.of(context).size.width/1.5-80,
+              //       height: MediaQuery.of(context).size.width/1.5-80,
+              //       decoration: BoxDecoration(
+              //         // borderRadius: BorderRadius.circular(100),
+              //         color: Colors.blue,
+              //         shape: BoxShape.circle,
+              //         boxShadow: <BoxShadow>[
+              //           BoxShadow(
+              //             color: Colors.black12,
+              //             spreadRadius: 8.0,
+              //             blurRadius: 8.0,
+              //           )
+              //         ],
+              //       ),
+              //     ),
+              //   ],
+              // ),
               Column(
                 children: [
                   Padding(
---
+++ lib/widgets/joystick.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+
+class Joystick extends StatefulWidget {
+  const Joystick({ Key? key }) : super(key: key);
+
+  @override
+  _JoystickState createState() => _JoystickState();
+}
+
+class _JoystickState extends State<Joystick> {
+  GlobalKey stackKey = GlobalKey();
+  late double stackLeft;
+  late double stackTop;
+  double innerCircleX = 75;
+  double innerCircleY = 75;
+
+  @override
+  void initState() {
+    WidgetsBinding.instance!
+        .addPostFrameCallback((_) {
+        RenderObject? box = stackKey.currentContext!.findRenderObject();
+            print(stackKey.currentContext!.findRenderObject());
+            var translation = box?.getTransformTo(null).getTranslation();
+            if (translation != null && box!.paintBounds != null) {
+              stackTop = box.paintBounds.shift(Offset(translation.x, translation.y)).top;
+              stackLeft = box.paintBounds.shift(Offset(translation.x, translation.y)).left;
+              // print("This is the stack Top coord ${stackTop} ");
+              // print("This is the stack Left coord ${stackLeft} ");
+            } else {
+              print(null);
+            }
+        });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onPanUpdate: (dragDetails) {
+        //TODO: check if it does not reach the end of the circle.
+        setState(() {
+          // print("This is the global position ${dragDetails.globalPosition}");
+          innerCircleX = dragDetails.globalPosition.dx - stackLeft - 75;
+          innerCircleY = dragDetails.globalPosition.dy - stackTop - 75;
+        });
+
+        
+      },
+      onPanEnd: (dragDetails){
+        setState(() {
+          innerCircleX = 75;
+          innerCircleY = 75;
+        });
+      },
+      child: Stack(
+        key: stackKey,
+        children: [
+          Container(
+            width: 300,
+            height: 300,
+            decoration: BoxDecoration(
+              color: Colors.blue,
+              shape: BoxShape.circle,
+              border: Border.all(
+                color: Colors.white,
+                width: 4.0,
+                style: BorderStyle.solid,
+              ),
+              boxShadow: <BoxShadow>[
+                BoxShadow(
+                  color: Colors.black12,
+                  spreadRadius: 8.0,
+                  blurRadius: 8.0,
+                )
+              ],
+            ),
+          ),
+          Positioned(
+            top: innerCircleY,
+            left: innerCircleX,
+            child: Container(
+            width: 150,
+            height: 150,
+            decoration: BoxDecoration(
+              color: Colors.blue,
+              shape: BoxShape.circle,
+              border: Border.all(
+                color: Colors.black26,
+                width: 2.0,
+                style: BorderStyle.solid,
+              ),
+              boxShadow: <BoxShadow>[
+                BoxShadow(
+                  color: Colors.black12,
+                  spreadRadius: 8.0,
+                  blurRadius: 8.0,
+                )
+              ],
+            ),
+          ),
+          )
+        ],
+      ),
+    );
+  }
+}
